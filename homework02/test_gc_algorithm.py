a824e39a3af6c322ebae1ceddbb7955e7c10cae2
Dana Kim <danakim@utexas.edu>
2024-02-05 23:56:41 -0600

    homework02

--- homework02/test_gc_algorithm.py
+++ homework02/test_gc_algorithm.py
@@ -7,7 +7,6 @@ def test_calculate_great_circle_distance():
         ((40.7128, -74.0060, 34.0522, -118.2437), 3964.686),
         ((0, 0, 0, 0), 0),  # Distance between the same point should be 0
         ((45.0, 0.0, -45.0, 180.0), 10007.543),  # Opposite points on the globe
-        # Add more test cases as needed
     ]
 
     for coordinates, expected_distance in test_cases:
