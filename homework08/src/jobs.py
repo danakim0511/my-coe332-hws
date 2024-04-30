9da61a9cf224a42a34e64e9f9bfac8aa81275005
Dana Kim <danakim@utexas.edu>
2024-04-30 01:09:10 -0500

    jobs.py

--- homework08/src/jobs.py
+++ homework08/src/jobs.py
@@ -38,6 +38,53 @@ def parse_xml_data(xml_file: str) -> dict:
 
     return data
 
+def _generate_jid():
+    """
+    Generate a pseudo-random identifier for a job.
+    """
+    return str(uuid.uuid4())
+
+def _instantiate_job(jid, status, start, end):
+    """
+    Create the job object description as a python dictionary. Requires the job id,
+    status, start and end parameters.
+    """
+    return {'id': jid,
+            'status': status,
+            'gene_id': start,   # change start and end
+            'gene_status': end }
+
+def _save_job(jid, job_dict):
+    """Save a job object in the Redis database."""
+    jdb.set(jid, json.dumps(job_dict))
+    return
+
+def _queue_job(jid):
+    """Add a job to the redis queue."""
+    q.put(jid)
+    return
+
+def add_job(start, end, status="submitted"):
+    """Add a job to the redis queue."""
+    jid = _generate_jid()
+    job_dict = _instantiate_job(jid, status, start, end)
+    _save_job(jid, job_dict)
+    _queue_job(jid)
+    return job_dict
+
+def get_job_by_id(jid):
+    """Return job dictionary given jid"""
+    return json.loads(jdb.get(jid))
+
+def update_job_status(jid, status):
+    """Update the status of job with job id `jid` to status `status`."""
+    job_dict = get_job_by_id(jid)
+    if job_dict:
+        job_dict['status'] = status
+        _save_job(jid, job_dict)
+    else:
+        raise Exception()
+
 def get_data(xml_file: str = 'SITE_HCC_FCT_DET.xml') -> dict:
     """
     Retrieve the healthcare center data from the XML file and return as a dictionary.
